06610597623cdb3c6bec7795435848333018d7a9
Dinh Tuan Anh <khanguyenan892@gmail.com>
2026-03-12 09:59:48 +0700

    first upload

---
+++ using system;.cs
@@ -0,0 +1,16 @@
+using system;
+using system.collections.generic;
+using system.linq;
+using system.text;
+
+namespace ConsoleApplication1
+{
+    class program{
+        static void Main (string[] args )
+        {
+            Console.Write("Hello World!");
+            Console.Readline();
+
+        }
+    }
+}
